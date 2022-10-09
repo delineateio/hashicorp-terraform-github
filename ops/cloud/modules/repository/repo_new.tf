1d28b7150070f1c7774760af53d9f48df976e4b7
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2022-10-09 23:38:32 +0000

    Tweak the branch merge strategies

--- ops/cloud/modules/repository/repo_new.tf
+++ ops/cloud/modules/repository/repo_new.tf
@@ -9,8 +9,8 @@ resource "github_repository" "this" {
   has_projects                            = false
   has_wiki                                = false
   allow_merge_commit                      = false
-  allow_squash_merge                      = true
-  allow_rebase_merge                      = false
+  allow_squash_merge                      = false
+  allow_rebase_merge                      = true
   squash_merge_commit_title               = "PR_TITLE"
   squash_merge_commit_message             = "PR_BODY"
   delete_branch_on_merge                  = true

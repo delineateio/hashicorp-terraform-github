5555ec80e5d9b6e22880d078daa358352fe6c218
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2023-07-04 20:04:28 +0000

    Resolve issue

--- ops/cloud/terraform.tf
+++ ops/cloud/terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = ">= 5.3.0, < 6.0.0"
+      version = ">= 5.9.0, < 6.0.0"
     }
     restapi = {
       source  = "registry.terraform.io/mastercard/restapi"
